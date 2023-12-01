1d033a3721cdf0661cc61090c6038c33d82a6314
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2023-11-30 19:44:13 -0600

    fixed the error

--- js/script.js
+++ js/script.js
@@ -1,80 +1,132 @@
+document.getElementById('current-btn').addEventListener('click', getCurrentLocation);
+document.getElementById('location-select').addEventListener('change', handleLocationChange);
+document.getElementById('search-box').addEventListener('input', handleLocationSearch);
+
+// Check if a location has been selected or if the current location button has been clicked
+let isLocationSelected = false;
+
+// Add this line to hide the data-table-container initially
+document.getElementById('data-table-container').style.display = 'none';
+
 function getCurrentLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successGetCurrentLocation, errorGetCurrentLocation);
-  } else {
-    showError("Geolocation is not supported by this browser.");
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            fetchSunriseSunset(latitude, longitude);
+        }, showError);
+    } else {
+        showError("Geolocation is not supported by this browser.");
+    }
+
+    // Update the flag when the current location button is clicked
+    isLocationSelected = true;
+
+    // Call the function to show or hide the data table
+    showDataTable();
 }
 
-function successGetCurrentLocation(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const apiEndpoint = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Chicago`;
-  fetchSunriseSunset(apiEndpoint);
+function handleLocationChange(event) {
+    const selectedValue = event.target.value;
+    if (selectedValue) {
+        const [latitude, longitude] = selectedValue.split(',');
+        fetchSunriseSunset(latitude, longitude);
+        isLocationSelected = true;
+
+        // Call the function to show or hide the data table
+        showDataTable();
+    }
 }
 
-function errorGetCurrentLocation(error) {
-  showError(`Error getting current location: ${error.message}`);
+function handleLocationSearch(event) {
+    const query = event.target.value;
+    fetch(`https://geocode.maps.co/search?q=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                const { lat, lon } = data[0];
+                fetchSunriseSunset(lat, lon);
+                isLocationSelected = true;
+
+                // Call the function to show or hide the data table
+                showDataTable();
+            } else {
+                showError("Location not found.");
+            }
+        }).catch(() => showError("Error fetching location data."));
 }
 
-function searchLocation() {
-  const searchInput = document.getElementById('searchLocation').value;
-  const selectedLocation = document.getElementById('locationSelect').value;
+function fetchSunriseSunset(latitude, longitude) {
+    const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = ''; // Clear existing data
 
-  if (selectedLocation === 'current') {
-    getCurrentLocation();
-  } else {
-    getSunriseSunset(searchInput);
-  }
+    for (let i = -1; i < 5; i++) {
+        setTimeout(() => {
+            let date = new Date();
+            date.setDate(date.getDate() + i);
+            fetchDataForDate(latitude, longitude, formatDate(date), i);
+        }, i * 500);
+    }
 }
 
-function getSunriseSunset(location) {
-  const geocodeApiEndpoint = `https://geocode.xyz/${encodeURIComponent(location)}?json=1`;
-  fetch(geocodeApiEndpoint)
-    .then(response => response.json())
-    .then(data => {
-      if (data.error) {
-        showError(`Error: ${data.error.description}`);
-      } else if (data.latt && data.longt) {
-        const latitude = data.latt;
-        const longitude = data.longt;
-        const apiEndpoint = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&timezone=America/Chicago`;
-        fetchSunriseSunset(apiEndpoint);
-      } else {
-        showError("Location not found");
-      }
-    })
-    .catch(error => showError(`Error searching location: ${error.message}`));
+function fetchDataForDate(latitude, longitude, date, dayIndex) {
+    const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=${date}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'OK') {
+                updateUI(data.results, dayIndex, formatDate(new Date(date)), latitude, longitude);
+            } else {
+                showError("Error fetching data.");
+            }
+        })
+        .catch(() => showError("Error fetching data."));
 }
 
-function fetchSunriseSunset(apiEndpoint) {
-  fetch(apiEndpoint)
-    .then(response => response.json())
-    .then(data => updateDashboard(data))
-    .catch(error => showError(`Error fetching sunrise and sunset data: ${error.message}`));
+function updateUI(data, dayIndex, formattedDate) {
+    let tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+    let newRow = tableBody.insertRow(tableBody.rows.length);
+
+    let cellDate = newRow.insertCell(0);
+    cellDate.innerHTML = formattedDate;
+
+    let cellTimezone = newRow.insertCell(1);
+    cellTimezone.innerHTML = data.timezone;
+
+    let cellSunrise = newRow.insertCell(2);
+    cellSunrise.innerHTML = data.sunrise;
+
+    let cellSunset = newRow.insertCell(3);
+    cellSunset.innerHTML = data.sunset;
+
+    let celldawn = newRow.insertCell(4);
+    celldawn.innerHTML = data.dawn;
+
+    let celldusk = newRow.insertCell(5);
+    celldusk.innerHTML = data.dusk;
+
+    let cellDayLength = newRow.insertCell(6);
+    cellDayLength.innerHTML = data.day_length;
+
+    let cellSolarNoon = newRow.insertCell(7);
+    cellSolarNoon.innerHTML = data.solar_noon;
 }
 
-function updateDashboard(data) {
-  const dashboard = document.getElementById('dashboard');
-  if (data.status === 'OK') {
-    const results = data.results;
-    dashboard.innerHTML = `
-      <h2>Results for ${results.formatted}</h2>
-      <p>Sunrise Today: ${results.sunrise}</p>
-      <p>Sunset Today: ${results.sunset}</p>
-      <p>Dawn Today: ${results.civil_twilight_begin}</p>
-      <p>Dusk Today: ${results.civil_twilight_end}</p>
-      <p>Day Length Today: ${results.day_length}</p>
-      <p>Solar Noon Today: ${results.solar_noon}</p>
-      <p>Timezone: ${results.timezone}</p>
-      <p>Powered by <a href="https://sunrisesunset.io/" target="_blank">SunriseSunset.io</a></p>
-    `;
-  } else {
-    showError(`Error: ${data.status}`);
-  }
+function formatDate(date) {
+    const options = { month: 'short', day: 'numeric', year: 'numeric' };
+    return new Intl.DateTimeFormat('en-US', options).format(date);
 }
 
-function showError(message) {
-  const dashboard = document.getElementById('dashboard');
-  dashboard.innerHTML = `<p class="error-message">${message}</p>`;
+function showError(error) {
+    const display = document.getElementById('data-display');
+    display.innerText = error;
+}
+
+// Add this function to show or hide the data table based on the location selection
+function showDataTable() {
+    const dataTableContainer = document.getElementById('data-table-container');
+    if (isLocationSelected) {
+        dataTableContainer.style.display = 'block';
+    } else {
+        dataTableContainer.style.display = 'none';
+    }
 }
